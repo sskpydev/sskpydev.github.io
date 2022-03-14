0043844c753381bc6cc477a56bff353315bc6991
sskpy1290 <sskpy1290@gmail.com>
2022-03-15 02:10:51 +0900

    Update File

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,7 @@
 $(function(){
+    /* ------------------------------
+    hamburger button
+    ------------------------------ */
     $('.toggle_btn').on('click', function() {
         if ($('#header').hasClass('open')) {
             $('#header').removeClass('open');
@@ -7,7 +10,28 @@ $(function(){
         }
     });
 
-    /* fadein */
+    $('#nav a').on('click', function() {
+        $('#header').removeClass('open');
+    })
+
+    $('#mask').on('click', function() {
+        $('#header').removeClass('open');
+    })
+
+    /* ------------------------------
+    smooth scroll
+    ------------------------------ */
+    $('a[href^="#"]').on('click', function() {
+        let href = $(this).attr('href');
+        let target = $(href == '#' || href == "" ? 'html' : href);
+        let position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, 1000, "swing")
+        return false;
+    });
+
+    /* ------------------------------
+    fadein
+    ------------------------------ */
     $(window).scroll(function() {
         $('.fadein').each(function() {
             let scroll = $(window).scrollTop();
