78383d55ffc7b60febbc0a7dbdd9550f95603e8a
sskpy1290 <sskpy1290@gmail.com>
2022-03-16 02:29:34 +0900

    Update File

--- docs/js/main.js
+++ docs/js/main.js
@@ -28,19 +28,4 @@ $(function(){
         $("html, body").animate({scrollTop:position}, 1000, "swing")
         return false;
     });
-
-    /* ------------------------------
-    fadein
-    ------------------------------ */
-    $(window).scroll(function() {
-        $('.fadein').each(function() {
-            let scroll = $(window).scrollTop();
-            let target = $(this).offset().top;
-            let windowHeight = $(window).height();
-            if (scroll > target - windowHeight + 200) {
-                $(this).css('opacity', '1');
-                $(this).css('transform', 'translateY(0)');
-            }
-        });
-    });
 });
