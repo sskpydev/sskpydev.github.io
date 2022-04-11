b07b767ba021afd37cf9660aa150ad66788fa545
sskpy1290 <sskpy1290@gmail.com>
2022-04-12 02:13:50 +0900

    Update File

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,26 +1,7 @@
 /* ------------------------------
-hamburger button
+fadeUp
 ------------------------------ */
-$('.toggle_btn').on('click', function() {
-    if ($('#header').hasClass('open')) {
-        $('#header').removeClass('open');
-    } else {
-        $('#header').addClass('open');
-    }
-});
-
-$('.nav-menu li a').on('click', function() {
-    $('#header').removeClass('open');
-});
-
-$('#mask').on('click', function() {
-    $('#header').removeClass('open');
-});
-
-/* ------------------------------
-fadeup
------------------------------- */
-function fadeAnime() {
+function fadeUp() {
     $('.fadeUpTrigger').each(function() {
         var scroll = $(window).scrollTop();
         var target = $(this).offset().top;
@@ -34,9 +15,43 @@ function fadeAnime() {
 };
 
 $(window).on('load', function() {
-    fadeAnime();
+    fadeUp();
 });
 
 $(window).scroll(function() {
-    fadeAnime();
+    fadeUp();
+});
+
+/* ------------------------------
+openButton
+------------------------------ */
+$(function() {
+    $('.toggle_btn').on('click', function() {
+        if ($('#header').hasClass('open')) {
+            $('#header').removeClass('open');
+        } else {
+            $('#header').addClass('open');
+        }
+    });
+
+    $('.nav-menu li a').on('click', function() {
+        $('#header').removeClass('open');
+    });
+
+    $('#mask').on('click', function() {
+        $('#header').removeClass('open');
+    });
+});
+
+/* ------------------------------
+smoothScroll
+------------------------------ */
+$(function() {
+    $('a[href*="#"]').click(function() {
+        var href = $(this).attr('href');
+        var target = $(href == '#' || href == '' ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, 500, "swing");
+        return false;
+    });
 });
